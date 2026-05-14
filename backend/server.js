ef4f8ba50bc1443eb6785ec4d44964c6c5303bf7
BDani99 <92380769+BDani99@users.noreply.github.com>
2026-05-14 12:28:32 +0200

    Revert "vercel"
    This reverts commit 98bd2862eadc2a7b13a715e661ef0a95ab47b63c.

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/assignments', assignmentRoutes);
 app.use('/api/student', studentRoutes);
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-module.exports = app;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
